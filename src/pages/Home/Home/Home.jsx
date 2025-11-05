24b805f6b159516a5765a2497f54482280a26ab6
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-11-05 18:30:30 +0600

    order now button scroll down set up

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,15 +1,21 @@
-import Banner from '../Banner/Banner'
-import Products from '../products/Products'
+import { useRef } from 'react';
+import Banner from '../Banner/Banner';
+import Products from '../products/Products';
 
 const Home = () => {
+  const productRef = useRef(null);
+  const scrollToProduct = () => {
+    productRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <>
-      <Banner></Banner>
-      <div className="max-w-screen-xl mx-auto">
+      <Banner ordernow={scrollToProduct}></Banner>
+      <div className="max-w-screen-xl mx-auto" ref={productRef}>
         <Products></Products>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
