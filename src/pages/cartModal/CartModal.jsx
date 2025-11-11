7cef1b8255eee44398d60a7d3a5a3b5a38dcfc89
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-11-11 19:13:14 +0600

    text color change

--- src/pages/cartModal/CartModal.jsx
+++ src/pages/cartModal/CartModal.jsx
@@ -31,7 +31,7 @@ const CartModal = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
-      <div className="bg-base-100 w-96 p-6 rounded-xl shadow-lg">
+      <div className="bg-base-100 text-white w-96 p-6 rounded-xl shadow-lg">
         <div className="flex justify-between">
           <h2 className="text-lg font-bold mb-4">Your Cart</h2>
           <button className="btn btn-error" onClick={onClose}>
