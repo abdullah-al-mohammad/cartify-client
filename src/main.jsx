fcd52d13058442b635edaadefff4891d2b947509
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-11-11 19:11:46 +0600

    add themeprovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Router/Router";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import AuthProvider from "./Router/provider/AuthProvider";
-import { CartProvider } from "./Router/provider/CartProvider";
-const queryClient = new QueryClient()
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
+import AuthProvider from './Router/provider/AuthProvider';
+import { CartProvider } from './Router/provider/CartProvider';
+import ThemeProvider from './Router/provider/ThemeProvider';
+import { router } from './Router/Router';
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </CartProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <CartProvider>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
+        </CartProvider>
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>
 );
