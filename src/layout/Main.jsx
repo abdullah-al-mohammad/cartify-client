1c72335b0b58759fd91356a8e1f2119fbeae7db0
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-11-13 13:06:00 +0600

    login register page navbar footer dont show

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -10,22 +10,30 @@ const Main = () => {
   const { cart } = useCart();
 
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const location = useLocation()
+  const location = useLocation();
+  const hideHeaderFooter = location.pathname === '/login' || location.pathname === '/register';
+  console.log(hideHeaderFooter);
+
   return (
     <>
-      <Navbar></Navbar>
+      {hideHeaderFooter || <Navbar />}
       <div className="min-h-screen">
-        {location.pathname === '/login' ? <Outlet></Outlet> : ''}
+        <Outlet />
       </div>
-      <Footer></Footer>
+      {hideHeaderFooter || <Footer />}
+
       {/* cart icon  */}
-      <button onClick={() => setIsCartOpen(true)}>
-        <div className="float-right indicator fixed right-7 bottom-40 bg-slate-100 p-2 rounded-full">
-          <img className="max-w-5" src={shopping} alt="" />
-          <span className="indicator-item badge bg-red-600">{cart.length}</span>
-        </div>
-      </button>
-      <CartModal isOpen={isCartOpen} onClose={() => setIsCartOpen(false)}></CartModal>
+      {hideHeaderFooter || (
+        <>
+          <button onClick={() => setIsCartOpen(true)}>
+            <div className="float-right indicator fixed right-7 bottom-40 bg-slate-100 p-2 rounded-full">
+              <img className="max-w-5" src={shopping} alt="" />
+              <span className="indicator-item badge bg-red-600">{cart.length}</span>
+            </div>
+          </button>
+          <CartModal isOpen={isCartOpen} onClose={() => setIsCartOpen(false)}></CartModal>
+        </>
+      )}
     </>
   );
 };
