6c2b454d51d9da423daca960b6f3c794b8201ddb
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2025-12-07 12:55:38 +0600

    import new css file name

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
+import './main.css';
 import AuthProvider from './provider/AuthProvider';
 import { CartProvider } from './provider/CartProvider';
 import { ThemeProvider } from './provider/ThemeProvider';
