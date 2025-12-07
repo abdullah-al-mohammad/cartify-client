9c0b73c19f1f7e27eb5c76cc74d5e3b9eeb76de5
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2025-12-07 13:14:16 +0600

    delete private route and change name adminRoute to private route

--- src/Router/PrivateRoute.jsx
+++ src/Router/PrivateRoute.jsx
@@ -1,21 +1,23 @@
 import { Navigate, useLocation } from 'react-router-dom';
+import useAdmin from '../hooks/useAdmin';
 import useAuth from '../hooks/useAuth';
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
+  const [isAdmin, isAdminLoading] = useAdmin();
   const location = useLocation();
 
-  if (loading) {
+  if (loading || isAdminLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
         <progress className="loading loading-spinner loading-xl"></progress>
       </div>
     );
   }
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location?.pathname }} replace />;
-  }
-  return children;
+
+  if (user && isAdmin) return children;
+
+  return <Navigate to="/login" state={{ from: location.pathname }} replace />;
 };
 
 export default PrivateRoute;
