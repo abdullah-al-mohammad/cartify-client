c08e2b141b884005060a3864086de98e409672b8
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2025-12-08 12:41:53 +0600

    remove unneccesery code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,7 @@ export default {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['poppins', 'sans-serif'],
-        rubik_storm: ['Rubik Storm', 'system-ui'],
-        oswald: ['Oswald', 'sans-serif'],
-        tagesschrift: ['Tagesschrift', 'system-ui'],
-      },
-      colors: {
-        blue2: ['#629dff'],
-      },
-    },
+    extend: {},
   },
   plugins: [require('daisyui')],
 };
