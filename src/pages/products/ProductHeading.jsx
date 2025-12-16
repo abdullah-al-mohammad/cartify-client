ecd03e35fd9a571ff6fb82a5fd6a5530a3941c9e
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2025-12-16 15:50:36 +0600

    change text size

--- src/pages/products/ProductHeading.jsx
+++ src/pages/products/ProductHeading.jsx
@@ -38,7 +38,7 @@ const ProductHeading = () => {
   return (
     <section id="product-section" className="py-20">
       <div className="text-center mb-5">
-        <h1 className="text-5xl mb-4 font-bold">Top Tech Deals</h1>
+        <h1 className="text-4xl mb-4">Top Tech Deals</h1>
         <p className="text-gray-600">
           Upgrade your lifestyle with the latest gadgets at unbeatable prices.
         </p>
